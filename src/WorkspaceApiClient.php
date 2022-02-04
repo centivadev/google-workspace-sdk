7fb0627cb92cd69cda8afb3ab8573f60ee650147
Dillon Wheeler <dwheeler@gitlab.com>
2022-02-04 10:15:26 -0500

    Update src/WorkspaceApiClient.php to include `DEFAULT_LOG_PATH` constant

--- src/WorkspaceApiClient.php
+++ src/WorkspaceApiClient.php
@@ -18,6 +18,7 @@ class WorkspaceApiClient
     // Standard parameters for building the ApiClient
     const BASE_URL = 'https://admin.googleapis.com/admin/directory/v1';
     const CONFIG_FILE_PATH = 'glamstack-google';
+    const DEFAULT_LOG_PATH = 'glamstack-google.connections.workspace.log_channels';
 
     private string $auth_token;
     private array $connection_config;
