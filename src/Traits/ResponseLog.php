8fb6cbac978e7cf463f72fe51dab412b08b3bf03
Dillon Wheeler <dwheeler@gitlab.com>
2022-02-04 09:34:33 -0500

    Update src/Traits/ResponseLog.php to add logInfo functionality

--- src/Traits/ResponseLog.php
+++ src/Traits/ResponseLog.php
@@ -35,4 +35,34 @@ trait ResponseLog{
             $this->logServerError($method, $url, $response);
         }
     }
+
+    /**
+     * Create an info log entry for an API call
+     *
+     * @param string $method
+     *      The lowercase name of the method that calls this function (ex. `get`)
+     *
+     * @param string $url
+     *      The URL of the API call including the concatenated base URL and URI
+     *
+     * @param object $response
+     *      The HTTP response formatted with $this->parseApiResponse()
+     *
+     * @return void
+     */
+    public function logInfo(string $method, string $url, object $response) : void
+    {
+        $message = Str::upper($method).' '.$response->status->code.' '.$url;
+
+        Log::stack((array) $this->connection_config['log_channels'])
+            ->info($message, [
+                'api_endpoint' => $url,
+                'api_method' => Str::upper($method),
+                'class' => get_class(),
+                'connection_key' => $this->connection_key,
+                'event_type' => 'google-workspace-api-response-info',
+                'message' => $message,
+                'status_code' => $response->status->code,
+            ]);
+    }
 }
