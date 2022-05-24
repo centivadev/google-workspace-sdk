0a09f523c747b6fa13c965cb69b5872db0ed11bc
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-24 16:17:50 -0400

    Add tests/TestCase.php

---
+++ tests/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Glamstack\GoogleWorkspace\Tests;
+
+use Glamstack\GoogleWorkspace\ApiClientServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+//        ini_set('memory_limit', '48M');
+        if(!is_dir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/storage/keys')){
+            mkdir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/storage/keys');
+        }
+        if (!is_dir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/storage/keys/glamstack-google')){
+            mkdir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/storage/keys/glamstack-google');
+        }
+        if (!is_link(__DIR__.'/../vendor/orchestra/testbench-core/laravel/storage/keys/glamstack-google/gcp_project_1.json')) {
+            symlink(__DIR__ . '/../storage/keys/glamstack-google/test.json', __DIR__.'/../vendor/orchestra/testbench-core/laravel/storage/keys/glamstack-google/gcp_project_1.json');
+        }
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ApiClientServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+    }
+}
