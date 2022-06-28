fe4223ba79cadff9a6c87d6a77eec1fb3a39991d
Dillon Wheeler <dwheeler@gitlab.com>
2022-06-28 10:11:19 -0400

    Add src/Resources/BaseClient.php
    Ref Issue #13

---
+++ src/Resources/BaseClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Glamstack\GoogleWorkspace\Resources;
+
+use Exception;
+use Glamstack\GoogleAuth\AuthClient;
+use Glamstack\GoogleWorkspace\ApiClient;
+use Glamstack\GoogleWorkspace\Traits\ResponseLog;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+
+abstract class BaseClient
+{
+}
