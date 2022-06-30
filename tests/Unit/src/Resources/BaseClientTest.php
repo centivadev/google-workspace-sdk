04409e800281d44a052e119917ef712c24d8929c
Dillon Wheeler <dwheeler@gitlab.com>
2022-06-30 15:09:34 -0400

    Update tests/Unit/src/Resources/BaseClientTest.php to add additional testing for methods
    Ref Issue #17, #13

--- tests/Unit/src/Resources/BaseClientTest.php
+++ tests/Unit/src/Resources/BaseClientTest.php
@@ -116,11 +116,19 @@ test('getConfigSubjectEmail() - it can get the subject email from configuration 
 });
 
 test('getConfigSubjectEmail() - it will set subject_email to null if not provided', function(){
-    $api_client = new ApiClientFake('test');
+    $api_client = new ApiClientFake('test_with_no_email');
     $method_client = new MethodFake($api_client);
     $method_client->setUp();
-    $subject_email = $method_client->getConfigSubjectEmail('test');
-    expect($subject_email)->toBe(config('tests.connections.test.subject_email'));
+    $subject_email = $method_client->getConfigSubjectEmail('test_with_no_email');
+    expect($subject_email)->toBe(null);
+});
+
+test('getConfigSubjectEmail() - it will set subject_email to null if parameter is missing', function(){
+    $api_client = new ApiClientFake('test_with_no_email_parameter');
+    $method_client = new MethodFake($api_client);
+    $method_client->setUp();
+    $subject_email = $method_client->getConfigSubjectEmail('test_with_no_email_parameter');
+    expect($subject_email)->toBe(null);
 });
 
 test('getConfigJsonFilePath() - it will get the json file path from configuration file', function(){
@@ -131,8 +139,13 @@ test('getConfigJsonFilePath() - it will get the json file path from configuratio
     expect($json_key_file_path)->toBe(config('tests.connections.test.json_key_file_path'));
 });
 
-test('getConfigJsonFilePath() - it will throw error if file_path is not set', function(){
+test('getConfigJsonFilePath() - it will throw error if file_path is null', function(){
     $api_client = new ApiClientFake('test_with_no_json_key_file_path');
+    $method_client = new MethodFake($api_client);
+})->expectExceptionMessage('The configuration file does not contain a json_key_file_path');
+
+test('getConfigJsonFilePath() - it will throw error if file_path set', function(){
+    $api_client = new ApiClientFake('test_with_no_json_key_file_path_parameter');
     $method_client = new MethodFake($api_client);
 })->expectExceptionMessage('The configuration file does not contain a json_key_file_path');
 
@@ -170,6 +183,23 @@ test('getConfigArraySubjectEmail() - it will get the subject email from config a
     $method_client = new MethodFake($api_client);
     $subject_email = $method_client->getConfigArraySubjectEmail($api_client->connection_config);
     expect($subject_email)->toBe(config('tests.connections.test.subject_email'));
+});
+
+test('getConfigArraySubjectEmail() - it will set the subject email to null if parameter is missing', function(){
+    $api_scopes = [
+        'https://www.googleapis.com/auth/admin.directory.group',
+        'https://www.googleapis.com/auth/contacts'
+    ];
+    $api_client = new ApiClientFake(null, [
+        'api_scopes' => $api_scopes,
+        'customer_id' => config('tests.connections.test.customer_id'),
+        'domain' => config('tests.connections.test.domain'),
+        'json_key_file_path' => storage_path('keys/glamstack-google-workspace/test.json'),
+        'log_channels' => ['single'],
+    ]);
+    $method_client = new MethodFake($api_client);
+    $subject_email = $method_client->getConfigArraySubjectEmail($api_client->connection_config);
+    expect($subject_email)->toBe(null);
 });
 
 test('getConfigArrayFilePath() - it will get the file path from config array', function(){
@@ -306,4 +336,127 @@ test('parseConnectionConfigArray() - it can parse the connection config array ap
     expect($array_contents)->toBe($expected_result);
 });
 
+test('getResponseBody() - it can get the response body', function(){
+    $api_client = new ApiClientFake('test');
+    $method_client = new MethodFake($api_client);
+    $method_client->setUp();
+    Http::fake([
+        '*' => Http::response([
+            'kind'       => 'admin#directory#group',
+            'id'         => '99999999',
+            'attributes' => [
+                'email'      => 'klibbygroup@exmaple.com',
+                'uuid'       => 'some uuid',
+                'created_at' => 'some created_at',
+                'updated_at' => 'some updated_at',
+            ],
+        ], 200, []),
+    ]);
+    $response = Http::get('*');
+    $response_object = $method_client->getResponseBody($response);
+    expect($response_object->id)->toBe('99999999');
+    expect($response_object->attributes->email == 'klibbygroup@exmaple.com');
+    expect(!property_exists($response_object, 'kind'))->toBeTrue();
+    expect(!property_exists($response_object, 'etag'))->toBeTrue();
+});
 
+test('getResponseBody() - it can get the response body and unset nextPageToken', function(){
+    $api_client = new ApiClientFake('test');
+    $method_client = new MethodFake($api_client);
+    $method_client->setUp();
+    Http::fake([
+        '*' => Http::response([
+            'kind'       => 'admin#directory#group',
+            'etag'       => 'etagid',
+            'id'         => '99999999',
+            'attributes' => [
+                'email'      => 'klibbygroup@exmaple.com',
+                'uuid'       => 'some uuid',
+                'created_at' => 'some created_at',
+                'updated_at' => 'some updated_at',
+            ],
+            'nextPageToken' => 'nextpagetokenid'
+        ], 200, []),
+    ]);
+    $response = Http::get('*');
+    $response_object = $method_client->getResponseBody($response);
+    expect($response_object->id)->toBe('99999999');
+    expect($response_object->attributes->email == 'klibbygroup@exmaple.com');
+    expect(!property_exists($response_object, 'nextPageToken'))->toBeTrue();
+});
+test('convertPaginatedResponseToObject() - it can convert paginated response to an object', function(){
+    $api_client = new ApiClientFake('test');
+    $method_client = new MethodFake($api_client);
+    $method_client->setUp();
+    $one = (object)[
+        'username' => 'klibby@example.com',
+        'phone_number' => '9999999999'
+    ];
+    $two = (object)[
+        'username' => 'kbilly@example.com',
+        'phone_number' => '9999999991'
+    ];
+    $response_example[0] = $one;
+    $response_example[1] = $two;
+    $paginated_object = $method_client->convertPaginatedResponseToObject($response_example);
+    expect($paginated_object)->toBeObject();
+    expect(collect($paginated_object)->flatten())->first()->username->toBe('klibby@example.com');
+});
+
+test('parseApiResponse() - it can parse non GET HTTP Responses', function(){
+    $api_client = new ApiClientFake('test');
+    $method_client = new MethodFake($api_client);
+    $method_client->setUp();
+    Http::fake([
+        '*' => Http::response([
+            'kind'       => 'admin#directory#group',
+            'id'         => '99999999',
+            'attributes' => [
+                'email'      => 'klibbygroup@exmaple.com',
+                'uuid'       => 'some uuid',
+                'created_at' => 'some created_at',
+                'updated_at' => 'some updated_at',
+            ],
+        ], 200, [
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'Server' => 'ESF'
+        ]),
+    ]);
+    $response = Http::get('*');
+    $parsed_response = $method_client->parseApiResponse($response);
+    expect($parsed_response->object->id)->toBe('99999999');
+    expect($parsed_response->status->code)->toBe(200);
+});
+
+test('parseApiResponse() - it can parse GET HTTP Responses', function(){
+    $api_client = new ApiClientFake('test');
+    $method_client = new MethodFake($api_client);
+    $method_client->setUp();
+    Http::fake([
+        '*' => Http::response([
+            'kind'       => 'admin#directory#group',
+            'id'         => '99999999',
+            'attributes' => [
+                'email'      => 'klibbygroup@example.com',
+                'uuid'       => 'some uuid',
+                'created_at' => 'some created_at',
+                'updated_at' => 'some updated_at',
+            ],
+            'nextPageToken' => 'nextpagetokenid'
+        ], 200, [
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'Server' => 'ESF'
+        ]),
+    ]);
+    $response = Http::get('*');
+    $response->results = $method_client->convertPaginatedResponseToObject(collect($method_client->getResponseBody($response))->toArray());
+    $parsed_response = $method_client->parseApiResponse($response, true);
+    expect($parsed_response->object->id)->toBe('99999999');
+    expect($parsed_response->object->attributes->email)->toBe('klibbygroup@example.com');
+});
+
+test('it will throw exception and log if bad initailization', function(){
+        $api_client = new ApiClientFake('test_with_incorrect_permissions');
+        $method_client = new MethodFake($api_client);
+        $method_client->setUp();
+})->expectExceptionCode(400);
