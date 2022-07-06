dbaa4c69c8c318c8f9ad7c042499d664ae2c617c
Dillon Wheeler <dwheeler@gitlab.com>
2022-07-06 09:49:04 -0400

    Update src/Resources/Rest/Rest.php to use `url` instead of `uri` parameters

--- src/Resources/Rest/Rest.php
+++ src/Resources/Rest/Rest.php
@@ -10,11 +10,12 @@ class Rest extends ApiClient
     /**
      * GET HTTP Request
      *
-     * This will perform a GET request against the provided `uri`. There is no
-     * validation for the provided URI or request data in this method.
+     * This will perform a GET request against the provided `url`. There is no
+     * validation for the provided URL or request data in this method. (i.e. 
+     * `https://admin.googleapis.com/admin/directory/v1/groups`)
      *
-     * @param string $uri
-     *      The Google URI to run the GET request with
+     * @param string $url
+     *      The Google URL to run the GET request with
      *
      * @param array $request_data
      *      Request data to load into GET request `Request Body`
@@ -61,20 +62,21 @@ class Rest extends ApiClient
      * ```
      * @throws Exception
      */
-    public function get(string $uri, array $request_data = []): object|string
+    public function get(string $url, array $request_data = []): object|string
     {
         $method = new Method($this);
-        return $method->get($uri, $request_data);
+        return $method->get($url, $request_data);
     }
 
     /**
      * POST HTTP Request
      *
-     * This will perform a POST request against the provided `uri`. There is no
-     * validation for the provided URI or request data in this method.
+     * This will perform a POST request against the provided `url`. There is no
+     * validation for the provided URL or request data in this method. (i.e
+     * `https://admin.googleapis.com/admin/directory/v1/groups`)
      *
-     * @param string $uri
-     *      The Google URI to run the POST request with
+     * @param string $url
+     *      The Google URL to run the POST request with
      *
      * @param array|null $request_data
      *      Request data to load into POST request `Request Body`
@@ -125,20 +127,21 @@ class Rest extends ApiClient
      *
      * @throws Exception
      */
-    public function post(string $uri, ?array $request_data = []): object|string
+    public function post(string $url, ?array $request_data = []): object|string
     {
         $method = new Method($this);
-        return $method->post($uri, $request_data);
+        return $method->post($url, $request_data);
     }
 
     /**
      * PATCH HTTP Request
      *
-     * This will perform a PATCH request against the provided `uri`. There is no
-     * validation for the provided URI or request data in this method.
+     * This will perform a PATCH request against the provided `url`. There is no
+     * validation for the provided URL or request data in this method. (i.e 
+     * `https://admin.googleapis.com/admin/directory/v1/groups`)
      *
-     * @param string $uri
-     *      The Google URI to run the PATCH request with
+     * @param string $url
+     *      The Google URL to run the PATCH request with
      *
      * @param array $request_data
      *      Request data to load into PATCH request `Request Body`
@@ -189,20 +192,21 @@ class Rest extends ApiClient
      *
      * @throws Exception
      */
-    public function patch(string $uri, array $request_data = []): object|string
+    public function patch(string $url, array $request_data = []): object|string
     {
         $method = new Method($this);
-        return $method->patch($uri, $request_data);
+        return $method->patch($url, $request_data);
     }
 
     /**
      * PUT HTTP Request
      *
-     * This will perform a PUT request against the provided `uri`. There is no
-     * validation for the provided URI or request data in this method.
+     * This will perform a PUT request against the provided `url`. There is no
+     * validation for the provided URL or request data in this method. (i.e
+     * `https://admin.googleapis.com/admin/directory/v1/groups`)
      *
-     * @param string $uri
-     *      The Google URI to run the PUT request with
+     * @param string $url
+     *      The Google URL to run the PUT request with
      *
      * @param array $request_data
      *      Request data to load into PUT request `Request Body`
@@ -248,20 +252,21 @@ class Rest extends ApiClient
      *
      * @throws Exception
      */
-    public function put(string $uri, array $request_data = []): object|string
+    public function put(string $url, array $request_data = []): object|string
     {
         $method = new Method($this);
-        return $method->put($uri, $request_data);
+        return $method->put($url, $request_data);
     }
 
     /**
      * DELETE HTTP Request
      *
-     * This will perform a DELETE request against the provided `uri`. There is no
-     * validation for the provided URI or request data in this method.
+     * This will perform a DELETE request against the provided `url`. There is no
+     * validation for the provided URL or request data in this method. (i.e 
+     * `https://admin.googleapis.com/admin/directory/v1/groups`)
      *
-     * @param string $uri
-     *      The Google URI to run the DELETE request with
+     * @param string $url
+     *      The Google URL to run the DELETE request with
      *
      * @param array $request_data
      *      Request data to load into DELETE request `Request Body`
@@ -297,9 +302,9 @@ class Rest extends ApiClient
      *
      * @throws Exception
      */
-    public function delete(string $uri, array $request_data = []): object|string
+    public function delete(string $url, array $request_data = []): object|string
     {
         $method = new Method($this);
-        return $method->delete($uri, $request_data);
+        return $method->delete($url, $request_data);
     }
 }
