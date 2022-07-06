ae74f164c9f998a86e835e2f45a67c4c75b2d2d9
Dillon Wheeler <dwheeler@gitlab.com>
2022-07-06 09:39:24 -0400

    Update src/Resources/Rest/Method.php to use `url` instead of `uri` parameters

--- src/Resources/Rest/Method.php
+++ src/Resources/Rest/Method.php
@@ -7,82 +7,82 @@ use Glamstack\GoogleWorkspace\Resources\BaseClient;
 class Method extends BaseClient
 {
     /**
-     * Run generic GET request on Google URI
+     * Run generic GET request on Google URL
      *
-     * @param string $uri
-     *      The URI to run the GET request on
+     * @param string $url
+     *      The URL to run the GET request on (i.e `https://admin.googleapis.com/admin/directory/v1/groups/<group_id>`)
      *
      * @param array $request_data
      *      Optional array data to pass into the GET request
      *
      * @return object|string
      */
-    public function get(string $uri, array $request_data = []): object|string
+    public function get(string $url, array $request_data = []): object|string
     {
-        return BaseClient::getRequest($uri, $request_data);
+        return BaseClient::getRequest($url, $request_data);
     }
 
     /**
-     * Run generic POST request on Google URI
+     * Run generic POST request on Google URL
      *
-     * @param string $uri
-     *      The URI to run the POST request on
+     * @param string $url
+     *      The URL to run the POST request on (i.e `https://admin.googleapis.com/admin/directory/v1/groups/<group_id>`)
      *
      * @param array|null $request_data
      *      Optional array data to pass into the POST request
      *
      * @return object|string
      */
-    public function post(string $uri, ?array $request_data = []): object|string
+    public function post(string $url, ?array $request_data = []): object|string
     {
-        return BaseClient::postRequest($uri, $request_data);
+        return BaseClient::postRequest($url, $request_data);
     }
 
     /**
-     * Run generic PATCH request on Google URI
+     * Run generic PATCH request on Google URL
      *
-     * @param string $uri
-     *      The URI to run the PATCH request on
+     * @param string $url
+     *      The URL to run the PATCH request on (i.e `https://admin.googleapis.com/admin/directory/v1/groups/<group_id>`)
      *
      * @param array $request_data
      *      Optional array data to pass into the PATCH request
      *
      * @return object|string
      */
-    public function patch(string $uri, array $request_data = []): object|string
+    public function patch(string $url, array $request_data = []): object|string
     {
-        return BaseClient::patchRequest($uri, $request_data);
+        return BaseClient::patchRequest($url, $request_data);
     }
 
     /**
-     * Run generic PUT request on Google URI
+     * Run generic PUT request on Google URL
      *
-     * @param string $uri
-     *      The URI to run the PUT request on
+     * @param string $url
+     *      The URL to run the PUT request on (i.e `https://admin.googleapis.com/admin/directory/v1/groups/<group_id>`)
      *
      * @param array $request_data
      *      Optional array data to pass into the PUT request
      *
      * @return object|string
      */
-    public function put(string $uri, array $request_data = []): object|string
+    public function put(string $url, array $request_data = []): object|string
     {
-        return BaseClient::putRequest($uri, $request_data);
+        return BaseClient::putRequest($url, $request_data);
     }
 
     /**
-     * Run generic DELETE request on Google URI
+     * Run generic DELETE request on Google URL
      *
-     * @param string $uri
-     *      The URI to run the DELETE request on
+     * @param string $url
+     *      The URL to run the DELETE request on (i.e `https://admin.googleapis.com/admin/directory/v1/groups/<group_id>`)
      *
      * @param array $request_data
      *      Optional array data to pass into the DELETE request
      *
      * @return object|string
      */
-    public function delete(string $uri, array $request_data = []): object|string
+    public function delete(string $url, array $request_data = []): object|string
     {
-        return BaseClient::deleteRequest($uri, $request_data);
+        return BaseClient::deleteRequest($url, $request_data);
     }
 }
