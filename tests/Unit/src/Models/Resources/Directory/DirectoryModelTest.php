6bcc94790a54ba2ec3f739b1b92af6a9829eada4
Dillon Wheeler <dwheeler@gitlab.com>
2022-07-13 11:51:46 -0400

    Update tests/Unit/src/Models/Resources/Directory/DirectoryModelTest.php to add test for setting the config array properly.

--- tests/Unit/src/Models/Resources/Directory/DirectoryModelTest.php
+++ tests/Unit/src/Models/Resources/Directory/DirectoryModelTest.php
@@ -37,3 +37,15 @@ test('verifyConfigArray() - it requires json_key or json_key_file_path to be set
     ]);
 })->expectExceptionMessage('Either the json_key_file_path or json_key parameters are required');
 
+test('verifyConfigArray() - it will set the config array properly', function(){
+    $directory_client = new Directory(null,[
+        'api_scopes' => ['fake_scope_1'],
+        'domain' => 'fake_domain',
+        'customer_id' => 'fake_id',
+        'json_key_file_path' => storage_path('a_file_path')
+    ]);
+    expect($directory_client->connection_config['domain'])->toBe('fake_domain');
+    expect($directory_client->connection_config['api_scopes'])->toBe(['fake_scope_1']);
+    expect($directory_client->connection_config['customer_id'])->toBe('fake_id');
+    expect($directory_client->connection_config['json_key_file_path'])->toBe(storage_path('a_file_path'));
+});
