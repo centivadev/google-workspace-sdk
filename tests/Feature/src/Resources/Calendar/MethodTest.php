d835f89f6bfc826cea6d05e3c64bbfb73c0dc361
Dillon Wheeler <dwheeler@gitlab.com>
2022-11-16 10:11:34 -0500

    Update tests/Feature/src/Resources/Calendar/MethodTest.php to add `post()` test
    Ref Issue #26

--- tests/Feature/src/Resources/Calendar/MethodTest.php
+++ tests/Feature/src/Resources/Calendar/MethodTest.php
@@ -10,3 +10,21 @@ test('get() - it can use GET to list calendars', function() {
     expect($response->status->code)->toBe(200)
         ->and(property_exists($response->object, 'items'))->toBeTrue();
 });
+
+test('post() - it can use POST to create calendar event', function(){
+    $api_client = new ApiClientFake('test');
+    $response = $api_client->calendar()->post(
+        '/calendars/' . config('tests.connections.test.subject_email') . '/events',
+        [
+            'start' => [
+                'date' => '2023-02-22',
+            ],
+            'end' => [
+                'date' => '2023-02-22',
+            ]
+        ]
+    );
+    expect($response->status->successful)->toBeTrue();
+});
+
+
