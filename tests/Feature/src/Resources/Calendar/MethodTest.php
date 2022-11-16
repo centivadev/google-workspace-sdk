15ccb0ba6d2aecbde965b809fe7e95b46c397627
Dillon Wheeler <dwheeler@gitlab.com>
2022-11-16 10:11:14 -0500

    Update tests/Feature/src/Resources/Calendar/MethodTest.php to add `get()` test

--- tests/Feature/src/Resources/Calendar/MethodTest.php
+++ tests/Feature/src/Resources/Calendar/MethodTest.php
@@ -3,3 +3,10 @@
 namespace Glamstack\GoogleWorkspace\Tests\Feature\src\Resources\Calendar;
 
 use Glamstack\GoogleWorkspace\Tests\Fakes\ApiClientFake;
+
+test('get() - it can use GET to list calendars', function() {
+    $api_client = new ApiClientFake('test');
+    $response = $api_client->calendar()->get('/users/me/calendarList');
+    expect($response->status->code)->toBe(200)
+        ->and(property_exists($response->object, 'items'))->toBeTrue();
+});
