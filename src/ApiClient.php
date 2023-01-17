d5458f74eb9c1edce2e3e1bb77c16c7878f78b96
Dillon Wheeler <dwheeler@gitlab.com>
2023-01-17 12:01:09 -0500

    Update The ApiClient Class To Add `getConfigArrayApiScopes` Method
    The `getConfigArrayApiScopes` method is used by the `parseConnectionConfigArray` method.
    
    The method previously lived inside of the `BaseClient` class.
    
    The method will return the `api_scopes` set in the `connection_config` array.

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -185,6 +185,19 @@ class ApiClient
             'json_key' => $this->getConfigArrayJsonKey($connection_config)
         ];
     }
+
+    /**
+     * Get the api_scopes from the connection_config array
+     *
+     * @param array $connection_config
+     *      The connection config array provided during initialization of the SDK
+     *
+     * @return array
+     */
+    protected function getConfigArrayApiScopes(array $connection_config): array
+    {
+        return $connection_config['api_scopes'];
+    }
     /**
      * Set the config path
      */
