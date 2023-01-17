ae42d3119681d5baade73c4371fda28cd38b94b0
Dillon Wheeler <dwheeler@gitlab.com>
2023-01-17 12:32:07 -0500

    Update The ApiClientFake Construct Method
    This commit update the `ApiClientFake` construct method to include an extra parameter called `authenticate`.
    
    The `authenticate` parameter will determine rather or not when running a test, if the SDK will
    attempt to authenticate with Google or not. The default is set to false, this allowed for not having to
    rework all of the previously written test cases.
    
    When running unit testing this should parameter should be set to true.
      - This allows for quicker testing as well as not having to provide a valid Google OAuth JSON key.

--- tests/Fakes/ApiClientFake.php
+++ tests/Fakes/ApiClientFake.php
@@ -2,10 +2,32 @@
 
 namespace Glamstack\GoogleWorkspace\Tests\Fakes;
 
+use Glamstack\GoogleAuth\AuthClient;
 use Glamstack\GoogleWorkspace\ApiClient;
+use Glamstack\GoogleWorkspace\Models\ApiClientModel;
 
 class ApiClientFake extends ApiClient
 {
+    public function __construct(?string $connection_key = null, ?array $connection_config = [], bool $authenticate = false)
+    {
+        if(!$authenticate){
+            parent::__construct($connection_key, $connection_config);
+        } else {
+            $api_client_model = new ApiClientModel();
+
+            $this->setConfigPath();
+
+            $this->setRequestHeaders();
+
+            if (empty($connection_config)) {
+                $this->setConnectionKey($connection_key);
+                $this->connection_config = [];
+            } else {
+                $this->connection_config = $api_client_model->verifyConfigArray($connection_config);
+                $this->connection_key = null;
+            }
+        }
+    }
 
     public function setConnectionKey(?string $connection_key): void
     {
